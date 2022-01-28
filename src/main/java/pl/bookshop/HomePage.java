a6073b40e6a383f37597471e37eaa438e5e925c2
pformela_ <p.formela.030@studms.ug.edu.pl>
2022-01-28 08:16:34 +0100

    initial commit

---
+++ src/main/java/pl/bookshop/HomePage.java
@@ -0,0 +1,16 @@
+package pl.bookshop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomePage {
+
+    @GetMapping
+    public String homePage() {
+        return "home";
+    }
+
+}
