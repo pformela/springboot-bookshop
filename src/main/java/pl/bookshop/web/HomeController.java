95b1abf1c92ad6ca4733c3ff40dda08c0036942e
pformela_ <p.formela.030@studms.ug.edu.pl>
2022-01-30 15:35:31 +0100

    added main page that displays all books in db

--- src/main/java/pl/bookshop/web/HomeController.java
+++ src/main/java/pl/bookshop/web/HomeController.java
@@ -1,15 +1,30 @@
 package pl.bookshop.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.bookshop.Book;
+import pl.bookshop.data.BookRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+    private BookRepository bookRepo;
+
+    public HomeController(BookRepository bookRepo) {
+        this.bookRepo = bookRepo;
+    }
+
     @GetMapping
-    public String home() {
+    public String home(Model model) {
+
+        model.addAttribute("books", bookRepo.findAll());
+
         return "home";
     }
 }
